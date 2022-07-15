c594e076658ad6b65f453c1b5954e4b9a5afe602
jyshtty <jyshtty@gmail.com>
2022-07-15 22:19:03 +0530

    BFS

--- DP-5/HW_Flip_array.py
+++ DP-5/HW_Flip_array.py
@@ -24,8 +24,6 @@ class Solution:
                         dp[i][j] = Node(curr_items, curr_weight)
                     else:
                         dp[i][j] = dp[i-1][j]
-
                 else:
                     dp[i][j] = dp[i-1][j]
-
         return dp[n][requiredSum].items;
