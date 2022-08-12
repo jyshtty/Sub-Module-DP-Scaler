675d27982aeddd0a7fcbf4090bee9a2f994b051c
jyshtty <56307643+jyshtty@users.noreply.github.com>
2022-08-12 15:56:58 +0530

    Update 0-1Knapsack.py

--- 0-1Knapsack.py
+++ 0-1Knapsack.py
@@ -1,3 +1,22 @@
+# # Given two integer arrays A and B of size N each which represent values and weights associated with N items respectively.
+
+# # Also given an integer C which represents knapsack capacity.
+
+# # Find out the maximum value subset of A such that sum of the weights of this subset is smaller than or equal to C.
+
+# # NOTE:
+
+# # You cannot break an item, either pick the complete item, or don’t pick it (0-1 property).
+
+
+#  A = [60, 100, 120]
+#  B = [10, 20, 30]
+#  C = 50
+    
+#  o/p - 220
+
+
+
 class Solution:
     # @param A : list of integers
     # @param B : list of integers
